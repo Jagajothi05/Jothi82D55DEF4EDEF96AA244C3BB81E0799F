828851eac7a75da07d5ab77a1b73190c15cbe48f
Jagajothi05 <144450409+Jagajothi05@users.noreply.github.com>
2023-10-05 14:35:08 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,9 +1,32 @@
-# Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
-
-# Add two numbers
-sum = float(num1) + float(num2)
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# Python program to create Bankaccount class
+# with both a deposit() and a withdraw() function
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+ 
+    def deposit(self):
+        amount=float(input("Enter amount to be Deposited: "))
+        self.balance += amount
+        print("\n Amount Deposited:",amount)
+ 
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+ 
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+ 
+# Driver code
+  
+# creating an object of class
+s = Bank_Account()
+  
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
